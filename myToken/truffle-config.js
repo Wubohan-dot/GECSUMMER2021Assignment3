a8673d9e8f4549ccf9e066118fb754e5ab76a314
postgre <819186192@qq.com>
2021-08-30 11:18:09 +0800

    myToken ->migrating to ganache successfully

--- myToken/truffle-config.js
+++ myToken/truffle-config.js
@@ -43,7 +43,7 @@ module.exports = {
     //
     development: {
      host: "127.0.0.1",     // Localhost (default: none)
-     port: 8545,            // Standard Ethereum port (default: none)
+     port: 7545,            // Standard Ethereum port (default: none)
      network_id: "*",       // Any network (default: none)
     },
     // Another network with more advanced options...
@@ -88,7 +88,7 @@ module.exports = {
          enabled: true,
          runs: 200
        },
-      //  evmVersion: "byzantium"
+       evmVersion: "byzantium"
       }
     }
   },
