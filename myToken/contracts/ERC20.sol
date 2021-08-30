8658796650d08649c1e13ed3b48397ec09b65edf
postgre <819186192@qq.com>
2021-08-30 10:55:20 +0800

    myToken compile rightly

--- myToken/contracts/ERC20.sol
+++ myToken/contracts/ERC20.sol
@@ -1,14 +1,14 @@
-pragma solidity 0.5.1;
+pragma solidity ^0.5.1;
 interface ERC20{
-    function name() public view returns (string);
-    function symbol() public view returns (string);
-    function decimals() public view returns (uint8);
-    function totalSupply() public view returns (uint256);
-    function balanceOf(address _owner) public view returns (uint256 balance);
-    function transfer(address _to, uint256 _value) public returns (bool success);
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
-    function approve(address _spender, uint256 _value) public returns (bool success);
-    function allowance(address _owner, address _spender) public view returns (uint256 remaining);
+    function name() external view returns (string memory);
+    function symbol() external view returns (string memory);
+    function decimals() external view returns (uint256);
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address _owner) external view returns (uint256 balance);
+    function transfer(address _to, uint256 _value) external returns (bool success);
+    function transferFrom(address _from, address _to, uint256 _value) external returns (bool success);
+    function approve(address _spender, uint256 _value) external returns (bool success);
+    function allowance(address _owner, address _spender) external view returns (uint256 remaining);
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 }
---
+++ myToken/contracts/XYZCoin.sol
@@ -0,0 +1,63 @@
+pragma solidity ^0.5.1;
+
+import "./ERC20.sol";
+
+contract XYZCoin is ERC20{
+    string private _name = "myToken";
+    string private _symbol = "WBH";
+    uint private _decimals = 0;
+    uint private _totalSupply = 1000;
+
+    //to get user's balance according to his address
+    mapping (address => uint256) public balances;
+
+    //to get the amount of money one user allows another user to get
+    mapping (address => mapping (address => uint256)) public allowed;
+
+    function name() public view returns (string memory){
+        return _name;
+    }
+    function symbol() public view returns (string memory){
+        return _symbol;
+    }
+    function decimals() public view returns (uint256){
+        return _decimals;
+    }
+    function totalSupply() public view returns (uint256){
+        return _totalSupply;
+    }
+
+    function balanceOf(address _owner) public view returns (uint256 balance){
+        return balances[_owner];
+    }
+    function transfer(address _to, uint256 _value) public returns (bool success){
+        require(balances[msg.sender]>=_value);
+        
+        balances[msg.sender]=balances[msg.sender]-_value;
+        balances[_to]=balances[_to]+_value;
+        
+        emit Transfer(msg.sender,_to,_value);
+        return true;
+    }
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success){
+        require(allowed[_from][msg.sender]>=_value);
+        require(balances[_from]>=_value);
+        
+        allowed[_from][msg.sender]=allowed[_from][msg.sender]-_value;
+        balances[_to]+=_value;
+        balances[_from]-=_value;
+        
+        emit Transfer(_from,_to,_value);
+        return true;
+    }
+    function approve(address _spender, uint256 _value) public returns (bool success){
+        require(balances[msg.sender]>=_value);
+        allowed[msg.sender][_spender]=_value;
+        emit Approval(msg.sender,_spender,_value);
+        return true;
+    }
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining){
+        return allowed[_owner][_spender];
+    }
+
+}
