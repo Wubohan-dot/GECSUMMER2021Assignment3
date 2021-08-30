37b532e2020bc560ccd5e9c33909691d84ff0441
postgre <819186192@qq.com>
2021-08-30 21:52:25 +0800

    myToken  EventandRevertTest successfully

--- myToken/test/RevertAndEventTest.js
+++ myToken/test/RevertAndEventTest.js
@@ -18,6 +18,15 @@ contract('XYZCoin',(accounts) =>{
             assert(error.message.indexOf('revert')>=0,"error message must contain revert");
         })
     })
+
+    it("Transferring from an account that has not explicitly authorized the transfer should revert the transaction",function(){
+        return XYZCoin.deployed().then(function(instance){
+            
+            return instance.transferFrom(accounts[0],accounts[2],120,{from:accounts[1]});
+        }).then(assert.fail).catch(function(error){
+            assert(error.message.indexOf('revert')>=0,"error message must contain revert");
+        })
+    })
     
 
     it("The transfer() and transferFrom() functions must fire the Transferevent",async()=>{
@@ -27,6 +36,13 @@ contract('XYZCoin',(accounts) =>{
 
         truffleAssert.eventEmitted(transTX,"Transfer");
         truffleAssert.eventEmitted(transFromTX,"Transfer");
+    })
+    
+    it("The approve() function must fire the Approval event",async()=>{
+        let apprTX=await xyzcoin.approve(accounts[6],20,{from:accounts[5]});
+
+        truffleAssert.eventEmitted(apprTX,"Approval");
+        
     })
 
 
